97acef5f9eaff57a6208dcd70b2536a968f8b3d2
Bishy <biswal.bishy1992@gmail.com>
2018-06-29 21:36:09 -0600

    Added the project

---
+++ pcfDemoApp/src/main/java/com/spring/pcf/pcfDemoApp/PcfDemoAppApplication.java
@@ -0,0 +1,12 @@
+package com.spring.pcf.pcfDemoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcfDemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcfDemoAppApplication.class, args);
+	}
+}
