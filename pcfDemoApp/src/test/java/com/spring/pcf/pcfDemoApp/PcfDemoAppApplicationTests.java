c8e44affdf0b3bffbb19a7f41bb0387983a7a439
Bishy <biswal.bishy1992@gmail.com>
2018-06-29 21:49:27 -0600

    adding the prj

---
+++ pcfDemoApp/src/test/java/com/spring/pcf/pcfDemoApp/PcfDemoAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.spring.pcf.pcfDemoApp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PcfDemoAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
